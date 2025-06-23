961caae0deb8cdc2004c943365d92ff334a7ec38
Ainadri Mandal <mandalainadri@gmail.com>
2025-06-23 22:32:02 +0530

    Update

--- Dictionary.py
+++ Dictionary.py
@@ -69,3 +69,26 @@ print("The updated dct_1 is after changing the keys in dict_2 : ",dct_1)
 merged = dct_1 | dct_2
 print("Merged dictionary using | operator (only works on py 3.9+) :",merged)
 
+#Dictionary methods
+# dict.clear() - Remove all the elements from the dictionary
+#
+# dict.copy() - Returns a copy of the dictionary
+#
+# dict.get(key, default = “None”) - Returns the value of specified key
+#
+# dict.items() - Returns a list containing a tuple for each key value pair
+#
+# dict.keys() - Returns a list containing dictionary’s keys
+#
+# dict.update(dict2) - Updates dictionary with specified key-value pairs
+#
+# dict.values() - Returns a list of all the values of dictionary
+# pop() - Remove the element with specified key
+#
+# popItem() - Removes the last inserted key-value pair
+#
+# dict.setdefault(key,default= “None”) - set the key to the default value if the key is not specified in the dictionary
+#
+# dict.has_key(key) - returns true if the dictionary contains the specified key.
+#
+# dict.get(key, default = “None”) - used to get the value specified for the passed key.
