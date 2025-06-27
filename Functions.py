dd0bfa3f061f16531fca95054c325d3fa8513a0e
Ainadri Mandal <mandalainadri@gmail.com>
2025-06-28 00:12:28 +0530

    Update

--- Functions.py
+++ Functions.py
@@ -21,7 +21,7 @@ def greet(n='-----'):
     print('Hello',n)
 
 greet('Ainadri')
-greet(str(input("Enter a string: ")))
+# greet(str(input("Enter a string: ")))
 greet()
 print('-'*50)
 
@@ -30,7 +30,7 @@ def Sum(a = 0,b = 0):
     print(a,b,a+b)
 Sum(5,6)
 Sum()
-Sum(int(input("Enter the value of a: ")), int(input("Enter the value of b: ")))
+# Sum(int(input("Enter the value of a: ")), int(input("Enter the value of b: ")))
 print('-'*50)
 
 #Return
@@ -50,6 +50,22 @@ print(s)
 print('-'*50)
 
 # Multilevel Functions
+lst = [1,2,3,4,5]
 
+def sq(lst):
+    return [i**2 for i in lst]
 
+def cu(lst):
+    return [i**3 for i in lst]
 
+print("Multilevel Functions")
+print(sq(lst))
+print(cu(lst))
+
+def sum_func():
+    lst_1 = sq(lst)
+    lst_2 = cu(lst)
+    return [lst_1[i] + lst_2[i] for i in range(len(lst_1))]
+print(sum_func())
+
+print('-'*50)
