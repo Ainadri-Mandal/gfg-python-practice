9a0c1ad7f2c2bb4893f61b086ac786710add7717
Ainadri Mandal <mandalainadri@gmail.com>
2025-06-27 22:18:20 +0530

    Update

--- Sets & Tuples.py
+++ Sets & Tuples.py
@@ -47,10 +47,57 @@ print('-'*50)
 print(my_set)
 my_set.clear()
 print(my_set)
+print('-'*50)
 
 
 #TUPLE
 
+tpl = (1,2,3,4,5)
+print(tpl)
+print('-'*50)
 
+#Accesing the value
 
+tpl = (1,2,3,4,5)
+print(tpl[0])
+print('-'*50)
+
+#Slicing
+tpl = (1,2,3,4,5)
+print(tpl[1:3])
+print('-'*50)
+
+#Concatenate
+tpl1 = (1,2,3,4)
+tpl2 = (4,5,6,7)
+tpl = tpl1 + tpl2
+print(tpl)
+print('-'*50)
+
+#Iterating
+print(tpl)
+for i in tpl:
+    print(i)
+print('-'*50)
+
+#Checking the Elements
+print(2 in tpl)
+print(3 in tpl)
+print(4 in tpl)
+print('-'*50)
+
+#Checking the count
+print(tpl)
+print(tpl.count(4))
+print('-'*50)
+
+#Finding the index
+print(tpl)
+print(tpl.index(4))
+print('-'*50)
+
+# Multiplying the tuple
+print(tpl)
+print(tpl*3)
+print('-'*50)
 
